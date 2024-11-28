10bf1769f74760826d9d9c8124458c00b5b05485
el <95074966+el-035@users.noreply.github.com>
2024-11-28 13:25:55 +0000

    started

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+#include <mlx.h>
+
 int main (void)
 {
-  
+  void *mlx_ptr;
+
+  mlx_ptr = mlx_init ();
+  mlx_new_window(mlx_ptr, 1000, 1000, "test");
 }
+
+mlx_new_window ( void *mlx_ptr, int size_x, int size_y, char *title );
